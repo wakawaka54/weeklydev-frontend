d7344e9ed7bae45024184973199f4f07018a044c
wakawaka54 <soue7@hotmail.com>
2016-08-23 22:16:10 -0500

    Modified ProjectModel to new API model

--- Models/Api/ProjectModel.cs
+++ Models/Api/ProjectModel.cs
@@ -11,10 +11,16 @@ namespace TestApp.Models.Api
         [JsonProperty(PropertyName = "_id", NullValueHandling = NullValueHandling.Ignore)]
         public string ID { get; set; }
 
+
         public string Title { get; set; }
         public string Description { get; set; }
+        public UserModel Creator { get; set; }
+        public bool Public { get; set; }
 
-
-        public DateTime Deadline { get; set; } = DateTime.Now.AddDays(14);
+        [JsonProperty(PropertyName = "created_on", NullValueHandling = NullValueHandling.Ignore)]
+        public DateTime CreatedOn { get; set; }
+        public DateTime Deadline { get; set; }
+        public string[] Tags { get; set; }
+        public int Votes { get; set; }
     }
 }
